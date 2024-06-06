158948ebbb6e122b42f1ccd5d23eca3ebdff23f0
Nana <naa4136779@gmail.com>
2024-06-06 16:17:57 +0800

    upload bug

--- general_member/doUpdateUser.php
+++ general_member/doUpdateUser.php
@@ -8,28 +8,11 @@ require_once("../db_connect.php");
 
 $id=$_POST["id"];
 $username=$_POST["username"];
-// $photo=$_POST["photo"];
-// echo $photo;
-// var_dump($_FILES);
-
-if($_FILES["file"]["error"]==0){
-    if(move_uploaded_file($_FILES["file"]["tmp_name"], "../upload/".$_FILES["file"]["name"])){
-        echo "upload success";
-    }else{
-        echo "upload failed";
-    }
-}
-
-$photo=$_FILES["file"]["tmp_name"];
 $password=$_POST["password"];
 $id_number=$_POST["id_number"];
 $birth_date=$_POST["birth_date"];
 $phone=$_POST["phone"];
 $email=$_POST["email"];
-exit;
-
-
-
 
 $sql="UPDATE users SET username='$username', password='$password', id_number='$id_number', birth_date='$birth_date', phone='$phone', email='$email' WHERE id=$id";
 
@@ -40,6 +23,6 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "更新資料錯誤: " . $conn->error;
 }
-header("location: user-edit.php?id=$id");
+header("location: user.php?id=$id");
 
 $conn->close();
